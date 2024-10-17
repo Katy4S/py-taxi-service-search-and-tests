c0ae2e17ac672fdeb23cdd7cdcd456d3c9b4247f
K. Petrychenko <catpetrychenko@gmail.com>
2024-10-18 01:52:08 +0300

    Solution

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from taxi.models import Driver, Car, Manufacturer
+
+
+class SearchTests(TestCase):
+    def setUp(self):
+        """Set up test data for the test case."""
+        self.driver1 = Driver.objects.create_user(
+            username="driver1",
+            password="testpass",
+            first_name="John",
+            last_name="Doe",
+            license_number="ABC12345"
+        )
+        self.driver2 = Driver.objects.create_user(
+            username="driver2",
+            password="testpass",
+            first_name="Jane",
+            last_name="Smith",
+            license_number="DEF12345"
+        )
+
+        self.manufacturer = Manufacturer.objects.create(name="Tesla",
+                                                        country="USA")
+        self.car = Car.objects.create(model="Model S",
+                                      manufacturer=self.manufacturer)
+
+        self.client.login(username="driver1", password="testpass")
+
+    def test_driver_search(self):
+        """Test searching for a driver by username."""
+        response = self.client.get(reverse("taxi:driver-list") + "?q=driver1")
+        self.assertContains(response, self.driver1.username)
+        self.assertNotContains(response, self.driver2.username)
+
+    def test_car_search(self):
+        """Test searching for a car by model."""
+        response = self.client.get(reverse("taxi:car-list") + "?q=Model S")
+        self.assertContains(response, self.car.model)
+
+    def test_manufacturer_search(self):
+        """Test searching for a manufacturer by name."""
+        response = self.client.get(reverse(
+            "taxi:manufacturer-list") + "?q=Tesla")
+        print(response.content.decode())
+        self.assertContains(response, "Tesla")
--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,11 @@
-from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
 from .models import Driver, Car, Manufacturer
 from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
@@ -12,11 +14,9 @@ from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
 @login_required
 def index(request):
     """View function for the home page of the site."""
-
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
-
     num_visits = request.session.get("num_visits", 0)
     request.session["num_visits"] = num_visits + 1
 
@@ -26,94 +26,154 @@ def index(request):
         "num_manufacturers": num_manufacturers,
         "num_visits": num_visits + 1,
     }
+    return render(request, "taxi/index.html",
+                  context=context)
 
-    return render(request, "taxi/index.html", context=context)
+
+class BaseListView(LoginRequiredMixin,
+                   generic.ListView):
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get("q")
+        if query:
+            queryset = queryset.filter(self.get_search_filter(query))
+        print(queryset)
+        return queryset
+
+    def get_search_filter(self, query):
+        raise NotImplementedError("Override this method in child classes")
 
 
-class ManufacturerListView(LoginRequiredMixin, generic.ListView):
+class DriverListView(BaseListView):
+    model = Driver
+    context_object_name = "driver_list"
+    template_name = "taxi/driver_list.html"
+    paginate_by = 10
+    ordering = ["username"]
+
+    def get_search_filter(self, query):
+        return Q(username__icontains=query)
+
+
+class CarListView(BaseListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+    template_name = "taxi/car_list.html"
+
+    def get_search_filter(self, query):
+        return Q(model__icontains=query)
+
+
+def get_search_filter(query):
+    return Q(name__icontains=query)
+
+
+class ManufacturerListView(ListView):
     model = Manufacturer
     context_object_name = "manufacturer_list"
     template_name = "taxi/manufacturer_list.html"
-    paginate_by = 5
+    paginate_by = 10
+    ordering = ["name"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get("q")
+        if query:
+            queryset = queryset.filter(get_search_filter(query))
+        return queryset
 
 
-class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
+class BaseCreateUpdateDeleteView(LoginRequiredMixin):
+    model = None
+    form_class = None
+    success_url = None
+    template_name_suffix = None
+
+
+class ManufacturerCreateView(BaseCreateUpdateDeleteView,
+                             generic.CreateView):
     model = Manufacturer
     fields = "__all__"
     success_url = reverse_lazy("taxi:manufacturer-list")
 
 
-class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
+class ManufacturerUpdateView(BaseCreateUpdateDeleteView,
+                             generic.UpdateView):
     model = Manufacturer
     fields = "__all__"
     success_url = reverse_lazy("taxi:manufacturer-list")
 
 
-class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
+class ManufacturerDeleteView(BaseCreateUpdateDeleteView,
+                             generic.DeleteView):
     model = Manufacturer
     success_url = reverse_lazy("taxi:manufacturer-list")
 
 
-class CarListView(LoginRequiredMixin, generic.ListView):
-    model = Car
-    paginate_by = 5
-    queryset = Car.objects.select_related("manufacturer")
-
-
-class CarDetailView(LoginRequiredMixin, generic.DetailView):
-    model = Car
-
-
-class CarCreateView(LoginRequiredMixin, generic.CreateView):
+class CarCreateView(BaseCreateUpdateDeleteView,
+                    generic.CreateView):
     model = Car
     form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
 
 
-class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
+class CarUpdateView(BaseCreateUpdateDeleteView,
+                    generic.UpdateView):
     model = Car
     form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
 
 
-class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
+class CarDeleteView(BaseCreateUpdateDeleteView,
+                    generic.DeleteView):
     model = Car
     success_url = reverse_lazy("taxi:car-list")
 
 
-class DriverListView(LoginRequiredMixin, generic.ListView):
-    model = Driver
-    paginate_by = 5
-
-
-class DriverDetailView(LoginRequiredMixin, generic.DetailView):
-    model = Driver
-    queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
-
-
-class DriverCreateView(LoginRequiredMixin, generic.CreateView):
+class DriverCreateView(BaseCreateUpdateDeleteView,
+                       generic.CreateView):
     model = Driver
     form_class = DriverCreationForm
+    success_url = reverse_lazy("taxi:driver-list")
 
 
-class DriverLicenseUpdateView(LoginRequiredMixin, generic.UpdateView):
+class DriverLicenseUpdateView(BaseCreateUpdateDeleteView,
+                              generic.UpdateView):
     model = Driver
     form_class = DriverLicenseUpdateForm
     success_url = reverse_lazy("taxi:driver-list")
 
 
-class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
+class DriverDetailView(LoginRequiredMixin,
+                       generic.DetailView):
     model = Driver
-    success_url = reverse_lazy("")
+    queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+
+
+class CarDetailView(LoginRequiredMixin,
+                    generic.DetailView):
+    """View to display details of a car."""
+    model = Car
+    template_name = "taxi/car_detail.html"
+
+
+class DriverDeleteView(LoginRequiredMixin,
+                       generic.DeleteView):
+    model = Driver
+    success_url = reverse_lazy("taxi:driver-list")
 
 
 @login_required
 def toggle_assign_to_car(request, pk):
+    """Toggle assignment of a car to a driver."""
     driver = Driver.objects.get(id=request.user.id)
-    if (
-        Car.objects.get(id=pk) in driver.cars.all()
-    ):  # probably could check if car exists
-        driver.cars.remove(pk)
+    car = get_object_or_404(Car, id=pk)
+
+    if car in driver.cars.all():
+        driver.cars.remove(car)
     else:
-        driver.cars.add(pk)
-    return HttpResponseRedirect(reverse_lazy("taxi:car-detail", args=[pk]))
+        driver.cars.add(car)
+
+    return HttpResponseRedirect(reverse_lazy("taxi:driver-list"))
